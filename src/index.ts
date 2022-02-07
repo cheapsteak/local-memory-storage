499f3252f2932a679ab77b1a7fe18fdcd7e5e1f1
Chang Wang <garnwraly@gmail.com>
2022-02-06 20:15:12 -0500

    run lint fix
    Signed-off-by: Chang Wang <garnwraly@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,72 @@
 export class LocalMemoryStorage implements Storage {
-    #data = new Map();
+  #data = new Map();
 
-    constructor() {
-        return new Proxy(this, {
-            // reveal keys in Reflect.ownKeys()
-            ownKeys: () => {
-                return Array.from(this.#data.keys())
-            },
+  constructor() {
+    return new Proxy(this, {
+      // reveal keys in Reflect.ownKeys()
+      ownKeys: () => {
+        return Array.from(this.#data.keys());
+      },
 
-            // reveal keys in Object.keys()
-            getOwnPropertyDescriptor: (target, prop) => {
-                const hasKey = target.#data.has(prop);
-                const value = hasKey ? target.getItem(prop) : undefined
-                return {
-                    value,
-                    writable: false,
-                    enumerable: hasKey,
-                    configurable: true,
-                };
-            },
+      // reveal keys in Object.keys()
+      getOwnPropertyDescriptor: (target, prop) => {
+        const hasKey = target.#data.has(prop);
+        const value = hasKey ? target.getItem(prop) : undefined;
+        return {
+          value,
+          writable: false,
+          enumerable: hasKey,
+          configurable: true,
+        };
+      },
 
-            // bind methods to target, otherwise calls that access private class fields 
-            // will have the proxy as their context and calls will fail with:
-            // "Cannot read private member from an object whose class did not declare it"
-            get: (target, name) => {
-                let ret = Reflect.get(target, name);
-                if (typeof ret === "function") {
-                    return ret.bind(target);
-                }
-                // otherwise retrieve the value of the corresponding key (e.g. for Object.values)
-                return this.getItem(name);
-            },
-            set: (target, name, value) => {
-                return Reflect.set(target, name, value);
-            }
-        })
-    }
-
-    get length(): number {
-        return this.#data.size;
-    }
-
-
-    key(index: number): string | null {
-        if (arguments.length === 0) {
-            throw new TypeError("MemoryWebStorage requires 1 argument, received 0.")
+      // bind methods to target, otherwise calls that access private class fields
+      // will have the proxy as their context and calls will fail with:
+      // "Cannot read private member from an object whose class did not declare it"
+      get: (target, name) => {
+        let ret = Reflect.get(target, name);
+        if (typeof ret === 'function') {
+          return ret.bind(target);
         }
-        return Array.from(this.#data.keys())[index]
-    }
+        // otherwise retrieve the value of the corresponding key (e.g. for Object.values)
+        return this.getItem(name);
+      },
+      set: (target, name, value) => {
+        return Reflect.set(target, name, value);
+      },
+    });
+  }
 
-    getItem(key: unknown): string | null {
-        return this.#data.has(key)
-            ? this.#data.get(key)
-            : null;
-    }
+  get length(): number {
+    return this.#data.size;
+  }
 
-    setItem(key: string, value: string): void {
-        this.#data.set(String(key), String(value));
+  key(index: number): string | null {
+    if (arguments.length === 0) {
+      throw new TypeError('MemoryWebStorage requires 1 argument, received 0.');
     }
+    return Array.from(this.#data.keys())[index];
+  }
 
-    removeItem(key: string): void {
-        this.#data.delete(key);
-    }
+  getItem(key: unknown): string | null {
+    return this.#data.has(key) ? this.#data.get(key) : null;
+  }
 
-    clear() {
-        this.#data.clear();
-    }
+  setItem(key: string, value: string): void {
+    this.#data.set(String(key), String(value));
+  }
 
-    get [Symbol.toStringTag]() {
-        return 'LocalMemoryStorage';
-    }
+  removeItem(key: string): void {
+    this.#data.delete(key);
+  }
+
+  clear() {
+    this.#data.clear();
+  }
+
+  get [Symbol.toStringTag]() {
+    return 'LocalMemoryStorage';
+  }
 }
 
 export const memoryStorage = new LocalMemoryStorage();
